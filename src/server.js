44230830cd5c9ad48c90489867283715f646ac19
akshit3022 <akshit2953z+free@gmail.com>
2026-03-28 08:37:45 +0000

    basic server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log("hello, Man")
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
